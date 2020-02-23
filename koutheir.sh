72500422a4703d34e14d9c58f057baa003bfe7dc
dan65536 <capnbearbossa@gmail.com>
2020-02-23 11:57:13 -0500

    files for multistage rr/gdb/valgrind build

---
+++ koutheir.sh
@@ -0,0 +1,25 @@
+
+apt-get update
+apt-get install make
+
+cd ~ ; git clone http://github.com/llvm/llvm-project
+
+cd llvm-project && \
+    mkdir build && \
+    cd build && \
+    /opt/irods-externals/cmake3.11.4-0/bin/cmake \
+        -G "Unix Makefiles"  -DLLVM_ENABLE_PROJECTS="libcxx;libcxxabi" ../llvm && \
+    make -j3 cxx cxxabi
+
+/opt/irods-externals/clang6.0-0/bin/clang++ -Wl,-rpath=$HOME/llvm-project/build/lib demo.cpp -stdlib=libc++ -g -O0 -std=c++17
+
+# -- Install & configure pretty-printers
+
+apt install -y python  # apt remove -y python3
+
+git clone https://github.com/koutheir/libcxx-pretty-printers
+
+: "
+PP_SRC_DIR=~/libcxx-pretty-printers/src
+ln -s $PP_SRC_DIR/gdbinit .gdbinit && sed -i.orig -e "s!<path.*>!$PP_SRC_DIR!" $PP_SRC_DIR/gdbinit
+"
