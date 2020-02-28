898616984d55dc576935f5b231719c1c8afd9a86
d-w-moore <dmoore@renci.org>
2020-02-28 15:59:57 -0500

    correct script name

--- build.bash
+++ build.bash
@@ -15,9 +15,8 @@ done
 shift $((OPTIND-1))
 
 if [ "${FORCE_TOOL_RECOMPILE+.}" = "." -o `docker image ls -q build_debuggers | wc -l` -lt 1 ]; then
-  $(dirname "$0")/bldtools.bash # - build rr,gdb,valgrind separately
+  $(dirname "$0")/build_debuggers.bash # - build rr,gdb,valgrind separately
 fi
-
 BLDDIR=$(dirname "$0")
 cd  "$BLDDIR"
 docker build --build-arg={login=`whoami`,uid=`id -u`,gid=`id -g`} "${TAGINFO[@]}" "$@" .
