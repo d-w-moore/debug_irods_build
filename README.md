c31145a2c2cb19ffb6f70d5018acb160eab7af22
dan65536 <capnbearbossa@gmail.com>
2020-07-22 08:59:39 -0400

    add usage doc to build script

--- README.md
+++ README.md
@@ -2,6 +2,23 @@
 
 # build for your user
 
-```
-docker build -t debug_my_irods --build-arg={login=`whoami`,uid=`id -u`,gid=`id -g`} .
-```
+  - see instructions printed by `./build.bash -h`
+
+Internally, running build.bash without options will
+  - build the debugging tools `gdb`, `rr`, and `valgrind` into a first image>
+    The process is to check (optionally force) that the `build_debuggers` image exists
+    and then do something like:
+  - build a second image called `use_debuggers` by default, with the executables for the
+    above-described debuggers copied in from the first
+    The image is built something like this, to preserve the host user's identity
+
+  ```
+  docker build -t debug_my_irods --build-arg={login=`whoami`,uid=`id -u`,gid=`id -g`} .
+  ```
+
+  - when running the `use_debuggers` image under control of `rr_docker.sh`, the
+  `~/github` directory (with iRODS source , etc) is mapped into the container
+
+  - Note that the  `rr_docker.sh` script uses the docker-run security/privilege options
+    that enable debuggers such as gdb and rr to function properly. Be sure to deploy 
+    these options yourself if running docker manually.
--- build.bash
+++ build.bash
@@ -2,9 +2,42 @@
 
 unset FORCE_TOOL_RECOMPILE
 TAGINFO=("-t" "use_debuggers:latest")
+usage() { cat<<-EOF
 
-while getopts  ":t:fF" opt; do
+	Creates: - a first docker image called "build_debuggers" in which
+		   gdb,rr, and valgrind are compiled from source
+		 - a second docker image called "use_debuggers" which
+		   imports the debugger executables from "build_debuggers"
+		   (This second image is meant to be used with rr_docker.sh,
+		   however you can optionally just steal the docker run
+		   options for debugging from there and write your own)
+
+	Building docker images:
+
+	`basename "$0"` [opts]
+
+	-[f|F] force build_debuggers to be rebuilt if not preexisting
+	       (F forces with docker's --no-cache option)
+
+	-t     rename the 2nd container (into which gdb,rr,valgrind are
+	       imported). The default name is "use_debuggers"
+
+	Sample build/usage steps:
+
+	\$ ./build.bash -F               # - to build both images
+
+	\$ ./rr_docker.sh use_debuggers  # - to run image with debugger
+	                                    executables available and
+	                                    ~/github mapped into container
+
+	Note - under Linux, /proc/sys/kernel/perf_event_paranoid may
+	       need to be set <= 1 on the host.  See: man sysctl
+	EOF
+  if [ $# -ge 1 ]; then exit $1; fi
+}
+while getopts  ":t:hfF" opt; do
     case $opt in
+      h) usage 0;;
       f) FORCE_TOOL_RECOMPILE="" ;;
       F) FORCE_TOOL_RECOMPILE="--no-cache" ;;
       t) TAGINFO=("-t" "$OPTARG") ;;
@@ -14,7 +47,9 @@ done
 
 shift $((OPTIND-1))
 
-if [ "${FORCE_TOOL_RECOMPILE+.}" = "." -o `docker image ls -q build_debuggers | wc -l` -lt 1 ]; then
+if [ "${FORCE_TOOL_RECOMPILE+.}" = "." \
+  -o `docker image ls -q build_debuggers | wc -l` -lt 1 ]
+then
   $(dirname "$0")/build_debuggers.bash $FORCE_TOOL_RECOMPILE # - build rr,gdb,valgrind separately
 fi
 BLDDIR=$(dirname "$0")
